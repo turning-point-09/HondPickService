1f73b1401a81ef0599f59b6ddf8e243c410bd568
Mayur Shete <mayurshete1331@gmail.com>
2025-06-23 19:32:39 +0530

    'do'

---
+++ src/main/java/com/example/handPick/HandPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.handPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/handPick/HandPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
